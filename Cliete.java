20dbe98a98f7857b94361a34a5905febf1f8b9e2
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-14 21:24:40 -0300

    Create Cliete.java

---
+++ Cliete.java
@@ -0,0 +1,14 @@
+// Classe que representa um cliente do banco
+public class Cliente {
+    private String nome;
+
+    // Construtor que define o nome do cliente
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    // Método para acessar o nome do cliente
+    public String getNome() {
+        return nome;
+    }
+}
